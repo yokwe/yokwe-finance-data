4702623deada59b75233ae42aeaa8e34e811a9df
hasegawa <hasegawa@localhost>
2025-08-01 22:07:58 +0900

    Simplify logic.

--- src/main/java/yokwe/finance/data/provider/yahoo/UpdateCompanyInfoUS.java
+++ src/main/java/yokwe/finance/data/provider/yahoo/UpdateCompanyInfoUS.java
@@ -9,13 +9,14 @@ import yokwe.finance.data.stock.us.StorageUS;
 import yokwe.finance.data.type.CompanyInfo;
 import yokwe.finance.data.type.StockInfoUS;
 import yokwe.util.Storage;
+import yokwe.util.ThreadUtil;
 
 
 public class UpdateCompanyInfoUS extends UpdateBase {
 	private static final org.slf4j.Logger logger = yokwe.util.LoggerUtil.getLogger();
 	
 	public static Makefile MAKEFILE = Makefile.builder().
-//		input(StorageUS.StockInfo). // FIXME circular dependency
+		input(StorageUS.StockInfo). // FIXME circular dependency
 		output(StorageYahoo.CompanyInfoUS).
 		build();
 	
@@ -31,35 +32,23 @@ public class UpdateCompanyInfoUS extends UpdateBase {
 			logger.info("start take {}", take);
 			int countMod = updateCompanyInfo();
 			if (countMod == 0) break;
-			try {
-				Thread.sleep(SLEEP_IN_MILLI);
-			} catch (InterruptedException e) {
-				//
-			}
+			
+			ThreadUtil.sleep(SLEEP_IN_MILLI);
 		}
 	}
 	private static final long    SLEEP_IN_MILLI = 1500;
 	
 	private int updateCompanyInfo() {
-		// read existing data
-		var list = StorageYahoo.CompanyInfoUS.getList().stream().collect(Collectors.toList());
-		logger.info("companyInfo  {}", list.size());
-		// remove if sector or industry is empty
-		list.removeIf(o -> o.sector.isEmpty() || o.industry.isEmpty());
-		logger.info("companyInfo  {}  remove if sector or industry is empty", list.size());
-		
 		// set of required stockCode
 		var stockInfoList = StorageUS.StockInfo.getList();
 		logger.info("stockInfo    {}", stockInfoList.size());
-		// remove if not stock
-		stockInfoList.removeIf(o -> !o.type.isStock());
-		logger.info("stockInfo    {}  remove if not stock", stockInfoList.size());
-		// remove if already processed
-		{
-			var set = list.stream().map(o -> o.stockCode).collect(Collectors.toSet());
-			stockInfoList.removeIf(o -> set.contains(o.stockCode));
-		}
-		logger.info("stockInfo    {}  remove if already processed", stockInfoList.size());
+		stockInfoList.removeIf(o -> o.type.isETF());
+		logger.info("stockInfo    {}  after remove ETF", stockInfoList.size());
+
+		var list = StorageYahoo.CompanyInfoUS.getList();
+		var existingSet = list.stream().map(o -> o.stockCode).collect(Collectors.toSet());
+		stockInfoList.removeIf(o -> existingSet.contains(o.stockCode));
+		logger.info("stockInfo    {}  after remove existing stock", stockInfoList.size());
 		
 		Collections.shuffle(stockInfoList);
 		int count = 0;
@@ -67,11 +56,7 @@ public class UpdateCompanyInfoUS extends UpdateBase {
 		for(var stockInfo: stockInfoList) {
 			if ((++count % 100) == 1) logger.info("{}  /  {}", count, stockInfoList.size());
 			
-			try {
-				Thread.sleep(SLEEP_IN_MILLI);
-			} catch (InterruptedException e) {
-				//
-			}
+			ThreadUtil.sleep(SLEEP_IN_MILLI);
 			
 			var stockCode = stockInfo.stockCode;
 			
@@ -81,7 +66,7 @@ public class UpdateCompanyInfoUS extends UpdateBase {
 			var sector   = companyInfo.sector.replace(",", "").replace("—", "-");
 			var industry = companyInfo.industry.replace(",", "").replace("—", "-");
 			
-			// skipe if sector or industry is empty
+			// skip if sector or industry is empty
 			if (sector.isEmpty() || industry.isEmpty()) continue;
 			
 			list.add(new CompanyInfo(stockCode, sector, industry));
